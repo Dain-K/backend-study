12933c80a4d0d017b01d2fc58fb061ed8cfb2d1b
Dain-K <dksudi76@naver.com>
2022-11-14 17:11:25 +0900

    db 연결

---
+++ spring-study/amgn/src/main/java/com/example/amgn/AmgnApplication.java
@@ -0,0 +1,13 @@
+package com.example.amgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmgnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmgnApplication.class, args);
+	}
+
+}
---
+++ spring-study/amgn/src/test/java/com/example/amgn/AmgnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amgn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmgnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
